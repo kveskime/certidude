25479977419894cb937c9fe7179c1f7e9bcf6a43
Karell Veskimeister <karell.veskimeister@itcollege.ee>
2017-02-02 09:51:32 +0200

    Changed tests/test_cli.py to unittest format

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,25 +1,23 @@
 from click.testing import CliRunner
 from certidude.cli import entry_point as cli
 from datetime import datetime, timedelta
+from certidude.api import certidude_app
+from falcon import testing
+from certidude import authority
+
 
 runner = CliRunner()
 
-def test_cli_setup_authority():
-    # Authority setup
-    # TODO: parent, common-name, country, state, locality
-    # {authority,certificate,revocation-list}-lifetime
-    # organization, organizational-unit
-    # pkcs11
-    # {crl-distribution,ocsp-responder}-url
-    # email-address
-    # inbox, outbox
+class TestCliSetup_TestAPI(testing.TestCase):
+    def setUp(self):
+        super(TestCliSetup_TestAPI, self).setUp()
+        self.app = certidude_app()
 
-    result = runner.invoke(cli, ['setup', 'authority'])
-    assert not result.exception
 
-    from certidude import authority
-    assert authority.certificate.serial_number == '0000000000000000000000000000000000000001'
-    assert authority.certificate.signed < datetime.now()
-    assert authority.certificate.expires > datetime.now() + timedelta(days=7000)
-
-        
+class TestMyApp(TestCliSetup_TestAPI):
+    def test_cli_setup_authority(self):
+        result = runner.invoke(cli, ['setup', 'authority'])
+        self.assertIsNot(result.exception)
+        self.assertEqual(authority.certificate.serial_number, '0000000000000000000000000000000000000001')
+        self.assertGreater(datetime.now(), authority.certificate.signed)
+        self.assertGreater(authority.certificate.expires, datetime.now() + timedelta(days=7000))
